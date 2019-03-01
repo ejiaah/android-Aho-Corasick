2c20a341ae9b9d75faf70dd4081897a4526c8047
ejiaah <46390343+ejiaah@users.noreply.github.com>
2019-03-01 16:29:24 +0900

    insertKeyword, parseText
    add insertKeyowrd method
    add parseText method

--- app/src/main/cpp/ahocorasick-lib.cpp
+++ app/src/main/cpp/ahocorasick-lib.cpp
@@ -14,9 +14,36 @@
 aho_corasick::wtrie trie;
 std::wstring converted_string;
 
+std::wstring convertJstringToWstring(JNIEnv *env, jstring string) {
+    std::wstring value;
+    if (string == NULL) {
+        return value; // empty string
+    }
+    const jchar *raw = env->GetStringChars(string, NULL);
+    if (raw != NULL) {
+        jsize len = env->GetStringLength(string);
+        value.assign(raw, raw + len);
+        env->ReleaseStringChars(string, raw);
+    }
+    return value;
+}
+
+jstring convertWstringToJstring(JNIEnv *env, std::wstring string) {
+    size_t len = string.length();
+    jchar *value_char = (jchar *) malloc((len + 1) * sizeof(jchar));
+    for (int i = 0; i < len; i++)
+        value_char[i] = string[i];
+    value_char[len] = 0;
+
+    jstring value = env->NewString(value_char, len);
+    delete[] value_char;
+    value_char = NULL;
+    return value;
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ejiaah_aho_1corasick_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
@@ -24,21 +51,67 @@ Java_com_ejiaah_aho_1corasick_MainActivity_stringFromJNI(
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_ejiaah_aho_1corasick_MainActivity_setCaseInsensitive(
-        JNIEnv* env,
+        JNIEnv *env,
+        jobject, /* this */
         jboolean isOn) {
     trie.set_case_insensitive(isOn);
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_ejiaah_aho_1corasick_MainActivity_setRemoveOverlaps(
-        JNIEnv* env,
+        JNIEnv *env,
+        jobject, /* this */
         jboolean isOn) {
     trie.set_remove_overlaps(isOn);
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_ejiaah_aho_1corasick_MainActivity_setOnlyWholeWords(
-        JNIEnv* env,
+        JNIEnv *env,
+        jobject, /* this */
         jboolean isOn) {
     trie.set_only_whole_words(isOn);
 }
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_ejiaah_aho_1corasick_MainActivity_insert(
+        JNIEnv *env,
+        jobject, /* this */
+        jstring keyword) {
+
+    if (keyword == NULL) {
+        return;
+    }
+
+    std::wstring wtext = convertJstringToWstring(env, keyword);
+    trie.insert(wtext);
+    LOGD("insert: %ls", wtext.c_str());
+}
+
+extern "C" JNIEXPORT jobjectArray JNICALL
+Java_com_ejiaah_aho_1corasick_MainActivity_parseText(
+        JNIEnv *env,
+        jobject, /* this */
+        jstring text) {
+    if (text == NULL) {
+        return NULL;
+    }
+
+    std::wstring wtext = convertJstringToWstring(env, text);
+    auto emits = trie.parse_text(wtext);
+
+    jobjectArray results = (jobjectArray) env->NewObjectArray(emits.size(),
+                                                              env->FindClass("java/lang/String"),
+                                                              env->NewStringUTF(""));
+    int i = 0;
+    for (const auto &emit : emits) {
+        std::wstring match = emit.get_keyword();
+        LOGD("emit: %ls", match.c_str());
+        env->SetObjectArrayElement(results, i, convertWstringToJstring(env, match));
+        i++;
+    }
+
+    return results;
+}
+
+
--- app/src/main/java/com/ejiaah/aho_corasick/MainActivity.java
+++ app/src/main/java/com/ejiaah/aho_corasick/MainActivity.java
@@ -6,12 +6,24 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    EditText insertEditText;
+    EditText parseEditText;
+    TextView resultTextView;
+
+    CheckBox configCaseInsensitiveCheckBox;
+    CheckBox configRemoveOverlapsCheckBox;
+    CheckBox configOnlyWholeWordsCheckBox;
 
     private RecyclerView recyclerView;
     private KeywordRecyclerAdapter keywordRecyclerAdapter;
@@ -26,6 +38,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertEditText = (EditText) findViewById(R.id.insert_editText);
+        parseEditText = (EditText) findViewById(R.id.parse_editText);
+        resultTextView = (TextView) findViewById(R.id.result_textView);
+
+        configCaseInsensitiveCheckBox = (CheckBox) findViewById(R.id.config_case_insensitive_checkBox);
+        configRemoveOverlapsCheckBox = (CheckBox) findViewById(R.id.config_remove_overlaps_checkBox);
+        configOnlyWholeWordsCheckBox = (CheckBox) findViewById(R.id.config_only_whole_words_checkBox);
 
         List<String> keywordList = new ArrayList<>();
         keywordList.add("he");
@@ -42,7 +62,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(keywordRecyclerAdapter);
 
-        setCaseInsensitive();
+        insert("he");
+        insert("she");
+        insert("his");
+        insert("hers");
     }
 
     /**
@@ -50,22 +73,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-    public native void setCaseInsensitive();
-    public native void setRemoveOverlaps();
-    public native void setOnlyWholeWords();
+
+    public native void setCaseInsensitive(boolean isOn);
+
+    public native void setRemoveOverlaps(boolean isOn);
+
+    public native void setOnlyWholeWords(boolean isOn);
+
+    public native void insert(String keyword);
+
+    public native String[] parseText(String text);
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.insert_button: {
-                EditText editText = (EditText) findViewById(R.id.insert_editText);
-                keywordRecyclerAdapter.insertItem(editText.getText().toString());
-                editText.setText("");
+                String keyword = insertEditText.getText().toString();
+                insertEditText.setText("");
+
+                keywordRecyclerAdapter.insertItem(keyword);
+                insert(keyword);
                 break;
             }
             case R.id.parse_button: {
+                String keyword = parseEditText.getText().toString();
+                parseEditText.setText("");
+
+                String[] results = parseText(keyword);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                if (results == null || results.length == 0) {
+                    stringBuilder.append("not matching");
+                } else {
+                    for (String result : results) {
+                        stringBuilder.append("matching: ").append(result).append("\n");
+                    }
+                }
+                resultTextView.setText(stringBuilder.toString());
+                break;
+            }
+            case R.id.config_case_insensitive_checkBox: {
+                setCaseInsensitive(configCaseInsensitiveCheckBox.isChecked());
+                break;
+            }
+            case R.id.config_remove_overlaps_checkBox: {
+                setRemoveOverlaps(configRemoveOverlapsCheckBox.isChecked());
+                break;
+            }
+            case R.id.config_only_whole_words_checkBox: {
+                setOnlyWholeWords(configOnlyWholeWordsCheckBox.isChecked());
                 break;
             }
         }
     }
+
+
 }
